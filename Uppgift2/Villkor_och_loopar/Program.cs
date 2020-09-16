d98d57c7934af5306551e7bfdcb4f21c0a7a7861
alexstenberg <70631861+alexstenberg@users.noreply.github.com>
2020-09-16 12:59:47 +0200

    Uppgift klar.

--- Uppgift2/Villkor_och_loopar/Program.cs
+++ Uppgift2/Villkor_och_loopar/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Diagnostics.Tracing;
+using System.Threading;
 namespace Villkor_och_loopar
 {
 
@@ -12,9 +13,96 @@ namespace Villkor_och_loopar
     {
         static void Main(string[] args)
         {
-            var x = Console.ReadLine();
 
-            
+            int counter = 0; // Antalet tävlande
+            int winNumber = 0;
+
+            int hourTime, minuteTime, secondTime;
+            int goalTime;
+
+            int bestHour = 0, bestMinute = 0, bestSecond = 0;
+            int bestTime = 0;
+
+            bool isRunning = true; // Bool för loop.
+
+            while (isRunning)
+            {
+                Thread.Sleep(1000);
+                Console.Clear();
+
+                Console.Write("\n\tAnge ett startnummer för den tävlande: ");
+                var startNumber = int.Parse(Console.ReadLine());
+
+                if (startNumber <= 0)
+                {
+                    isRunning = false;
+                    continue;
+                }
+
+                counter++; // Antalet tävlande.
+             
+                // Mata in timma för start / minut för start / sekund för start
+                Console.WriteLine("\nAnge timme för starttid (0-23): ");
+                var startHour = int.Parse(Console.ReadLine());
+                Console.WriteLine("\nAnge minut för starttid(0-59): ");
+                var startMinute = int.Parse(Console.ReadLine());
+                Console.WriteLine("\nAnge sekund för starttid(0-59): ");
+                var startSecond = int.Parse(Console.ReadLine());
+
+                // Mata in timma för mål / minut för mål / sekund för mål
+                Console.WriteLine("\nAnge timme för mål(0-23): ");
+                var goalHour = int.Parse(Console.ReadLine());
+                Console.WriteLine("\nAnge minut för mål(0-59): ");
+                var goalMinute = int.Parse(Console.ReadLine());
+                Console.WriteLine("\nAnge sekund för mål(0-59): ");
+                var goalSecond = int.Parse(Console.ReadLine());
+
+                int startInSeconds = (startHour * 3600) + (startMinute * 60) + startSecond; // Starttid på dygnet i sekunder
+                int goalInSeconds = (goalHour * 3600) + (goalMinute * 60) + goalSecond; // Måltid på dygnet i sekunder
+
+                if (startInSeconds > goalInSeconds) // Om midnatt passeras
+                {
+                    goalInSeconds = (86400 + goalInSeconds);
+                }
+
+                goalTime = goalInSeconds - startInSeconds;
+
+                hourTime = (goalTime / 3600);
+                minuteTime = (goalTime % 3600) / 60;
+                secondTime = (goalTime % 60);
+
+                Console.WriteLine($"\nTimmar: {hourTime}" +
+                                  $"\nMinuter: {minuteTime} " +
+                                  $"\nSekunder {secondTime}");
+                
+
+                if (goalTime < bestTime || counter == 1)
+                {
+                    bestTime = goalTime;
+                    winNumber = startNumber;
+                    bestHour = hourTime;
+                    bestMinute = minuteTime;
+                    bestSecond = secondTime;
+                }
+
+            }
+
+            if (winNumber == 0)
+            {
+                Console.Clear();
+                Console.WriteLine("\nInga tävlande!");
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine($"\nAntal tävlande: {counter}");
+                Console.WriteLine($"\nVinnaren är: {winNumber}");
+                Console.WriteLine($"\nVinnande sluttid är: {bestHour} Timmar {bestMinute} Minuter {bestSecond} Sekunder.");
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
+
--- Uppgift3/Klasser/Program.cs
+++ Uppgift3/Klasser/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Xml;
 
 namespace Klasser
 {
@@ -10,14 +14,75 @@ namespace Klasser
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            bool showMenu = true;
+            List<string> allCars = new List<string>();
 
-            Console.WriteLine("Hello World!");
+
+
+            while (showMenu)
+            {
+                showMenu = MainMenu();
+            }
+            
+        }
+
+        public static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("[1] Registrera ny bil");
+            Console.WriteLine("[2] Visa alla registrerade bilar");
+            Console.WriteLine("[3] ");
+            Console.WriteLine("[4] ");
+            Console.WriteLine("[5] Exit");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    return true;
+                case "2":
+                    return true;
+                case "3":
+                    return true;
+                case "4":
+                    return true;
+                case "5":
+                    return false;
+                default:
+                    return true;
+            }
+
         }
     }
 
-    public class Bil
+    public class Car
     {
-        public string Registreringsnummer { get; set; }
+        public int Carweight { get; set; }
+        private DateTime Regdate { get; set; }
+        public bool Electric { get; set; }
+
+        public string carModel;
+        private int carMiles;
+
+        public Car(string carModel, int carMiles)
+        {
+            this.carModel = carModel;
+            this.carMiles = carMiles;
+        }
+
+        public int GetCarMiles()
+
+        public void ElectricYesNo()
+        {
+            if (Electric == true)
+            {
+                Console.WriteLine("\n\tDetta är en elbil.");
+            }
+            else
+            {
+                Console.WriteLine("\n\tDetta är inte en elbil.");
+            }
+        }
+
 
     }
 }
